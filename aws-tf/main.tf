fd9ef85fed048d545be99e56d0db0758750f35f0
aviclouder <avinshar.cloud@gmail.com>
2025-12-06 16:15:40 +0530

    Initial setup

--- aws-tf/main.tf
+++ aws-tf/main.tf
@@ -1,24 +1,15 @@
 provider "aws" {
-  region = var.region
+    # profile = var.profile
+    region = var.region
 }
-
 
 module "vpc" {
-  source = "./modeules/networking/vpc"
-  prefix = var.prefix
-  env = var.env
-  cidr_block = var.vpc_cidr
+    source = "./modules/networking/vpc"
+    cidr_block = var.cidr_block
+
 }
 
-
 module "subnets" {
-  source = "./modeules/networking/subnets"
-  vpc_cidr = var.vpc_cidr
-
-  vpc_id =  module.vpc.vpc_id
-  prefix = var.prefix
-  region = var.region
-  public_subnet_count = var.public_subnet_count
-  private_subnet_count = var.private_route_table_mode
-  public_route_table_mode = var.public_route_table_mode
+    source = "./modeules/networking/subnets"
+    
 }
--- aws-tf/variables.tf
+++ aws-tf/variables.tf
@@ -1,66 +1,27 @@
 variable "region" {
     type = string
-    description = "AWS default region"
-}
-
-variable "prefix" {
-    type = "string"
-    description = "Value that should be used in prefix of the service name"
-}
-
-variable "use_default_vpc" {
-    type = bool
-    description = "Check if you want to use default VPC or not"
-    default = false  
-}
-
-variable "vpc_cidr" {
-    default = "10.0.0.0/16"
-    type = string
-    description = "CIDR block for AWS VPC"
-}
-
-variable "public_subnet_count" {
-  description = "Number of public subnets to create"
-  type        = number
-  default     = 3
-}
-
-variable "private_subnet_count" {
-  description = "Number of private subnets to create"
-  type        = number
-  default     = 3
-}
-
-
-variable "k8s_cluster_name" {
-    default = "k8s"
-    type = string
-    description = "Kubernetes Cluster Name"   
-}
-
-
-variable "public_route_table_mode" {
-  description = "Route table mode: shared or dedicated"
-  type        = string
-  default     = "shared"  # options: shared or dedicated
-}
-
-
-variable "private_route_table_mode" {
-  description = "Route table mode for private subnets: shared or dedicated"
-  type        = string
-  default     = "shared"  # options: shared or dedicated
+    description = "aws region"
+    default = "us-east-1"
 }
 
 
 variable "env" {
     type = string
-    description = "Environment for AWS Infra. Select from: DEV, STG, PROD or keep it blank(using double quotes)"
+    description = "Name of environment for which the resource is being created"
+    default = "dev"     # dev/qa/stg/prod
 }
 
-variable "nat_gateway_mode" {
-  description = "single or per_az"
-  type        = string
-  default     = "single"
+
+
+variable "cidr_range" {
+    type = string
+    description = "CIDR range for VPC"
+    default = "10.10.0.0/16"
+}
+
+
+variable "eks_cluster" {
+    type = string
+    description = "cluster name for eks"
+    default = "demo-eks"
 }
